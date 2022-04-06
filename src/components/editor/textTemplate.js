09a6c8a4d69bf5a13dcf2d26484384cc08987e28
raja <raja.rakoto7@gmail.com>
2022-04-06 20:49:41 +0300

    add image template

--- src/components/editor/textTemplate.js
+++ src/components/editor/textTemplate.js
@@ -1,3 +1,5 @@
+const image =
+	'<img align="right" src="http://octodex.github.com/images/minion.png" alt="drawing" width="200"/>\n\n';
 const title =
 	'# h1 Heading\n## h2 Heading\n### h3 Heading\n#### h4 Heading\n##### h5 Heading\n###### h6 Heading';
 const paragraphes =
@@ -5,7 +7,8 @@ const paragraphes =
 const specs = ' *italique* | **gras** | \n`monospace` | ~~rayé~~.';
 const list =
 	'Liste:\n\n  - javascript\n\t+ react\n\t+ vue\n\t+ angular\n - python\n  - bash';
-const listNum = 'Liste numérotée:\n\n  1. node.js\n  2. mongodb\n  3. express\n  4. next.js ';
+const listNum =
+	'Liste numérotée:\n\n  1. node.js\n  2. mongodb\n  3. express\n  4. next.js ';
 const links = 'Lien facebook : https://web.facebook.com/raja.rakotonirina ';
 const checklist = 'Checklist:\n\n- [x] task 1\n- [x] task 2\n- [ ] task 3';
 const code = '```js\n console.log("Miarahaba !"); \n``` ';
@@ -13,6 +16,7 @@ const code = '```js\n console.log("Miarahaba !"); \n``` ';
 const separate = '\n\n---\n\n';
 
 export const textTemplate =
+	image +
 	title +
 	separate +
 	paragraphes +
