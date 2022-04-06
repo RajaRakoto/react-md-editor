ff2acf9344b0acb5d0149a74e4f9f269998b0567
raja <raja.rakoto7@gmail.com>
2022-04-06 15:03:53 +0300

    fix responsive

--- src/components/editor/editor.js
+++ src/components/editor/editor.js
@@ -4,30 +4,30 @@ import './editor.scss';
 export default class Editor extends Component {
 	render() {
 		return (
-			<div class="container">
+			<div class="container-fluid">
 				<div class="row">
-					<div class="col-md-6">
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
 						<div id="area_text">
 							<label for="input">Input:</label>
 							<br></br>
 							<textarea
 								id="input"
 								name="input"
-								rows="30"
-								cols="60"
+								rows="25"
+								cols="40"
 								className="form-control z-depth-1"
 							></textarea>
 						</div>
 					</div>
-					<div class="col-md-6">
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
 						<div id="area_md">
 							<label for="output">Output:</label>
 							<br></br>
 							<textarea
 								id="output"
 								name="output"
-								rows="30"
-								cols="60"
+								rows="25"
+								cols="40"
 								className="form-control z-depth-1"
 							></textarea>
 						</div>
