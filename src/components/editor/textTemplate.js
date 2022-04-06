2ba3845e5f00d654899287c85eb1b0a56f8ce099
raja <raja.rakoto7@gmail.com>
2022-04-06 19:28:01 +0300

    refactor textTemplate

--- src/components/editor/textTemplate.js
+++ src/components/editor/textTemplate.js
@@ -1,2 +1,14 @@
+const title = '# Titre\n## Sous-Titre \n### Titre plus profond\n \n';
+const paragraphes =
+	"Les paragraphes sont séparés\npar une ligne vide.\n\nLaissé deux espaces à la fin d'une ligne pour  \naller à la ligne.\n\n";
+const specs = ' *italique* | **gras** | \n`monospace` | ~~rayé~~.\n\n';
+const list = 'Liste:\n\n  * javascript\n  * python\n  * bash\n\n';
+const listNum =
+	'Liste numérotée:\n\n  1. node.js\n  2. mongodb\n  3. react\n\n ';
+const links = 'Lien facebook : https://web.facebook.com/raja.rakotonirina \n\n';
+const checklist =
+	'Checklist:\n\n- [x] task 1\n- [x] task 2\n- [ ] task 3   \n\n';
+const code = '```js\n console.log("Miarahaba !"); \n``` ';
+
 export const textTemplate =
-	'# Titre\n## Sous-Titre \n### Titre plus profond\n \nLes paragraphes sont séparés\npar une ligne vide.\n\nLaissé deux espaces à la fin d\'une ligne pour  \naller à la ligne.\n\nAttributs *italique*, **gras**, \n`monospace`, ~~rayé~~.\n\nListe:\n\n  * javascript\n  * python\n  * bash\n\nListe numérotée:\n\n  1. node\n  2. mongodb\n  3. npm\n\n *[Raja Rakoto](https://github.com/RajaRakoto/)* \n\n Lien facebook : https://web.facebook.com/raja.rakotonirina \n\n```js\n console.log("Miarahaba !"); \n``` ';
+	title + paragraphes + specs + list + listNum + links + checklist + code;
