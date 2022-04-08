e4488a7daecfe06b453dede3fbfada7b140de373
raja <raja.rakoto7@gmail.com>
2022-04-08 13:52:43 +0300

    valid imgmin & reorganize

--- gruntfile.js
+++ gruntfile.js
@@ -1,50 +1,90 @@
 module.exports = function (grunt) {
-	require('load-grunt-tasks')(grunt);
+	require('load-grunt-tasks')(grunt); // grunt plugins loader
+
+	// all files destination
+	const sassDistDestination = './src/dist/styles/';
+	const imageDistDestination = './src/dist/images/';
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('./package.json'),
 
-		// concat: {
-		// 	dist: {
-		// 		src: ['src/index.scss', 'src/components/editor/editor.scss'],
-		// 		dest: 'src/dist/style.min.scss',
-		// 	},
-		// },
-
-		jshint: {
-			options: {
-				esversion: 11,
-				strict: false,
+		// TODO: testing
+		/**
+		 * Conatenate files
+		 */
+		concat: {
+			dist: {
+				src: ['./src/index.scss', './src/components/editor/editor.scss'],
+				dest: './src/dist/style.min.scss',
 			},
-			all: ['./gruntfile.js', './src/index.js', './src/components/**/*.js'],
 		},
 
+		// TODO: verified
+		/**
+		 * Validate js files
+		 */
+		jshint: {
+			options: {
+				esversion: 11, // ecmascript version for jshint
+				strict: false, // strict mode
+			},
+			all: ['./gruntfile.js', './src/index.js', './src/components/**/*.js'], // js file to verify
+		},
+
+		// TODO: verified
+		/**
+		 * Compile sass to css
+		 */
 		sass: {
 			dist: {
 				options: {
-					style: 'compressed',
+					style: 'compressed', // output style: compact, expanded, compressed
 				},
 				files: [
+					// scss file list
 					{
 						src: ['./src/index.scss'],
-						dest: './src/dist/index.min.css',
+						dest: sassDistDestination + 'index.min.css',
 					},
 					{
 						src: ['./src/components/editor/editor.scss'],
-						dest: './src/dist/editor.min.css',
+						dest: sassDistDestination + 'editor.min.css',
 					},
 				],
 			},
 		},
 
+		// TODO: verified
+		/**
+		 * Minify & optimize all images
+		 */
+		imagemin: {
+			dynamic: {
+				files: [
+					{
+						expand: true,
+						cwd: './src/assets/images/', // img source
+						src: ['**/*.{png,jpg,gif,svg}'], // img extension
+						dest: imageDistDestination, // img destination
+					},
+				],
+			},
+		},
+
+		// TODO: verified
+		/**
+		 * Run predefined tasks whenever watched file patterns are added, changed or deleted
+		 * cmd: grunt watch
+		 */
 		watch: {
 			dist: {
-				files: ['./src/*.scss', './src/components/**/*.scss'],
-				tasks: ['default'],
-				options: { spawn: false },
+				files: ['./src/*.scss', './src/components/**/*.scss'], // scss file list to watch
+				tasks: ['sass-task'], // default task to execute
+				options: { spawn: false }, // watch optimization
 			},
 		},
 	});
-
-	grunt.registerTask('default', ['sass:dist']);
+	grunt.registerTask('jshint-task', ['jshint']);
+	grunt.registerTask('imagemin-task', ['imagemin']);
+	grunt.registerTask('sass-task', ['sass:dist']);
 };
